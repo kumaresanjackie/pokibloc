fd10a344a378c588522a894c98f7f33c0ef3e74f
kumaresan <kumaresanjackie@gmail.com>
2024-06-29 13:07:25 +0530

    Name Changed

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokemon',
       theme: ThemeData(
         // This is the theme of your application.
         //
